56201d36b27fd8a2a6b68e5f4d7eeb804e6a424a
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-07-14 10:51:36 -0700

    add firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,16 @@ import { LineupComponent } from './lineup/lineup.component';
 import { AboutComponent } from './about/about.component';
 import { routing } from './app.routing';
 import { PersonDetailComponent } from './person-detail/person-detail.component';
-// import { PersonComponent } from './person-service/person.component';
+import { masterFireBaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFireBaseConfig.apiKey,
+  authDomain: masterFireBaseConfig.authDomain,
+  databaseURL: masterFireBaseConfig.databaseURL,
+  storageBucket: masterFireBaseConfig.storageBucket
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +29,9 @@ import { PersonDetailComponent } from './person-detail/person-detail.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
