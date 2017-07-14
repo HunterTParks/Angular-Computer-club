356b4c3857f087f1f89c10f18f3933a5e97bc64f
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-07-14 09:05:02 -0700

    add more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LineupComponent } from './lineup/lineup.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineupComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
